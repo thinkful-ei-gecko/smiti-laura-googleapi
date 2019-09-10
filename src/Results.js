dcd890e931fd75c210b4327bf9b4c4996b77bcf1
laura <laura.j.elias@gmail.com>
2019-09-10 17:14:02 -0500

    almost complete

--- src/Results.js
+++ src/Results.js
@@ -3,13 +3,22 @@ import './Results.css';
 
 
 export default function Results(props){
-    
+    console.log(props);
 
-    return ( 
-    
-    //this.props.state.
-    <section className='results'>
-      <p>results</p>
-    </section>
+ let resultsArray = this.props.state.books.items.map(item => <div className='book'>
+        <p>{item.volumeInfo.title}</p>
+        <p>{item.volumeInfo.authors[0]}</p>
+        <p>Price: {item.saleInfo.listPrice.amount}</p>
+        <p>Description: {item.volumeInfo.description}</p>
+        <a href='{item.volumeInfo.previewLink}' >Preview Link</a>
+        <img src='{item.volumeInfo.imageLinks.smallThumbnail}' alt='book cover' height='50' />
+        </div>);
+
+  let resultsDisplay = resultsArray.join('');
+
+    return (
+      <section className='results'>
+        ${resultsDisplay}
+      </section>
     );
 }
